2ba68618d44dd212a571acbb835ef53544a4fc52
Chanturidze <george.chanturidze@gmail.com>
2018-03-21 00:02:14 -0700

    complete game JS functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,41 +6,64 @@ var minCrystal = 1;
 var maxCrystal = 12;
 var minRandom = 19;
 var maxRandom = 120;
+var totalScore = 0;
+var currentCrystalValue = 0;
+var randomNumber;
 
+//Randomize crystal values between min & max values
 function getRndCrystal(minCrystal, maxCrystal) {
     return Math.floor(Math.random() * (maxCrystal - minCrystal + 1) ) + minCrystal;
 }
 
+//Randomize the random number user sees at the start of the game
 function getRndNumber(minRandom, maxRandom) {
     return Math.floor(Math.random() * (maxRandom - minRandom + 1) ) + minRandom;
 }
 
-
-var randomCrystal1;
-var randomCrystal2;
-var randomCrystal3;
-var randomCrystal4;
-var randomNumber;
-
-//function to start a game on each win/lose
+//function to start the game on each win/lose
 function startGame() {
-    //randomize crystal numbers and random number on each game start
+    //reset total score to zero
+    totalScore = 0;
 
-    var randomCrystal1 = getRndCrystal(minCrystal, maxCrystal);
-    console.log(randomCrystal1);
-    
-    var randomCrystal2 = getRndCrystal(minCrystal, maxCrystal);
-    console.log(randomCrystal2);
+    //randomize random number on each game start & display in div
+    randomNumber = getRndNumber(minRandom, maxRandom);
+    $('#random-num').html('Random Number: ' + randomNumber);
 
-    var randomCrystal3 = getRndCrystal(minCrystal, maxCrystal);
-    console.log(randomCrystal3);
-
-    var randomCrystal4 = getRndCrystal(minCrystal, maxCrystal);
-    console.log(randomCrystal4);
-
-    var randomNumber = getRndNumber(minRandom, maxRandom);
-    console.log(randomNumber);
+    //attach random crystal number to each crystal divs 
+    $(".crystal").each(function(){
+        $(this).attr("data-value",getRndCrystal(minCrystal, maxCrystal));
+    });
 }
+
+//gather value of clicks on each crystal
+$('.crystal').click(function() {
+    //get the value of currently clicked crystal
+    currentCrystalValue = parseInt($(this).attr('data-value'));
+
+    //calculate current total score
+    totalScore += currentCrystalValue;
+    $('#score').html('Your Total Score is: ' + totalScore);
+
+    //if total score exceeds random number, count as a loss and restart the game
+    if (totalScore > randomNumber) {
+        losses++;
+        startGame();
+        $('#losses').html('Losses: ' + losses);    
+        $('#score').html('Your Total Score is: ' + totalScore);
+    }
+
+    //if total score equals random number, count as a win and restart the game
+    else if (totalScore === randomNumber) {
+        wins++;
+        startGame();
+        $('#wins').html('Wins: ' + wins);      
+        $('#score').html('Your Total Score is: ' + totalScore);
+    }
+
+    else {
+        return false;
+    }
+});
 
 startGame();
 
